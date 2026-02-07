96d24dcfea61f256ee5719946bc17feb041d6ef1
RNunes69 <rafa.nunes03@hotmail.com>
2026-02-06 23:45:45 -0300

    criacao da class DadosEpisodio

---
+++ screen-match/src/main/java/br/com/rafael/ScreenMatch/model/DadosEpisodio.java
@@ -0,0 +1,7 @@
+package br.com.rafael.ScreenMatch.model;
+
+public record DadosEpisodio(String titulo,
+                            Integer numero,
+                            String avaliacao,
+                            String dataLancamento) {
+}
