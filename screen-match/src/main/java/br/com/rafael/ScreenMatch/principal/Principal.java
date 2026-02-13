cdbbb34ddd8f98913da1521de2da958c4882ad91
RNunes69 <rafa.nunes03@hotmail.com>
2026-02-12 23:55:08 -0300

    Atualizando class principal

--- screen-match/src/main/java/br/com/rafael/ScreenMatch/principal/Principal.java
+++ screen-match/src/main/java/br/com/rafael/ScreenMatch/principal/Principal.java
@@ -6,7 +6,17 @@ public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
 
+    private final String endereco;
+    private final String apiKey;
+
+    public Principal(String endereco, String apiKey) {
+        this.endereco = endereco;
+        this.apiKey = apiKey;
+    }
+
     public void exibMenu(){
-        System.out.println("Digite o nome da série para a busca");
+        System.out.println("https://omdbapi.com/?t=impuros&season=1&episode=1&apikey=5fffbc20&");
+        var nomeSerie = leitura.nextLine();
+        //"https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c"
     }
 }
