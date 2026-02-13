32a89b170c727a888a2a0b05f76f135a3ba778bf
RNunes69 <rafa.nunes03@hotmail.com>
2026-02-12 23:51:14 -0300

    Criancao da class principal

---
+++ screen-match/src/main/java/br/com/rafael/ScreenMatch/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.com.rafael.ScreenMatch.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    public void exibMenu(){
+        System.out.println("Digite o nome da série para a busca");
+    }
+}
